ea36e26c6e1e6bbfaa793ed02e0aaa2c7ab8d8d3
SVXTECH <116860869+SVXTECH@users.noreply.github.com>
2024-08-22 11:11:38 +0800

    re

--- next.config.mjs
+++ next.config.mjs
@@ -23,10 +23,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = {
-  experimental: {
-    serverActions: true,
-  },
-};
-
 export default bundleAnalyzer(withPWA(nextConfig));
